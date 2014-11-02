dbfe4eec229dc1baf7d1d04e2cf8195543f29a44
Wilbur Yu <yuwilbur@gmail.com>
2014-11-01 17:30:37 -0700

    Added arrow cursors

---
+++ Assets/Scripts/Scroll/ScrollCursor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VRWidgets
+{
+  public class ScrollCursor : MonoBehaviour
+  {
+    public GameObject cursor = null;
+    public GameObject incIndicator = null;
+    public GameObject decIndicator = null;
+
+    private float upper_limit_ = 0.0f;
+    private float lower_limit_ = 0.0f;
+
+    private float previous_percent_ = -1.0f;
+
+    public void SetLimits(float upper_limit, float lower_limit) 
+    {
+      upper_limit_ = upper_limit;
+      lower_limit_ = lower_limit;
+    }
+
+    public void UpdatePercent(float percent)
+    {
+      if (previous_percent_ < 0.0f)
+        previous_percent_ = percent;
+
+      if (cursor != null)
+      {
+        Vector3 local_position = cursor.transform.localPosition;
+        local_position.y = (upper_limit_ - lower_limit_) * percent + lower_limit_;
+        cursor.transform.localPosition = local_position; 
+      }
+
+      incIndicator.SetActive(false);
+      decIndicator.SetActive(false);
+      if (percent > previous_percent_ && incIndicator != null)
+      {
+        incIndicator.SetActive(true);
+      } 
+      else if (percent < previous_percent_ && decIndicator != null)
+      {
+        decIndicator.SetActive(true);
+      }
+
+      previous_percent_ = percent;
+    }
+  }
+}
+
--- Assets/Scripts/Scroll/ScrollViewer.cs
+++ Assets/Scripts/Scroll/ScrollViewer.cs
@@ -8,7 +8,7 @@ namespace VRWidgets
     public ScrollContent scrollContent;
     public GameObject scrollBoundaries;
     public GameObject scrollWindow;
-    public GameObject scrollCursor;
+    public ScrollCursor scrollCursor;
 
     private Limits boundaries_ = new Limits();
 
@@ -18,15 +18,12 @@ namespace VRWidgets
       boundaries_.GetLimitsToLocal(scrollBoundaries, gameObject);
       scrollWindow.transform.localPosition = new Vector3((boundaries_.r + boundaries_.l) / 2.0f, (boundaries_.t + boundaries_.b) / 2.0f, 0.0f);
       scrollWindow.transform.localScale = new Vector3((boundaries_.r - boundaries_.l), (boundaries_.t - boundaries_.b), 0.0f);
+      scrollCursor.SetLimits(boundaries_.t, boundaries_.b);
     }
 
     void LateUpdate()
     {
-      float percent = scrollContent.content_percentage;
-      float position = (boundaries_.t - boundaries_.b) * percent + boundaries_.b;
-      Vector3 scroll_position = scrollCursor.transform.localPosition;
-      scroll_position.y = position;
-      scrollCursor.transform.localPosition = scroll_position;
+      scrollCursor.UpdatePercent(scrollContent.content_percentage);
     }
   }
 }
